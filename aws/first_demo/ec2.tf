d3511a885db0025b13b4754280dbd7a101fec477
Gagandeep Singh <admin.gagan@gmail.com>
2021-10-29 15:26:48 +0530

    Update ec2.tf

--- aws/first_demo/ec2.tf
+++ aws/first_demo/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "myawsserver" {
   instance_type = "t2.nano"
 
   tags = {
-    Name = "Gagandeep-aws-ec2-instance-v2"
+    Name = "Gagandeep-aws-ec2-instance-v3"
     Env = "Dev"
   }
 }
